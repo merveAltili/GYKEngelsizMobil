38f431a558feb494a95eabf11ad6932f6959d6af
Merve Altılı <mervealtili@gmail.com>
2017-09-28 19:08:16 +0300

    ön_girisV1

---
+++ app/src/main/java/com/gelecegiyazanlar/mervegulsah/engelsizmobil/OnGiris.java
@@ -0,0 +1,29 @@
+package com.gelecegiyazanlar.mervegulsah.engelsizmobil;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class OnGiris extends AppCompatActivity {
+
+    Button btnDernek,btnKullanici;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_giris);
+
+        btnDernek= (Button) findViewById(R.id.btnDernek);
+
+        btnDernek.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(OnGiris.this,Giris.class);
+                startActivity(i);
+            }
+        });
+    }
+
+}
